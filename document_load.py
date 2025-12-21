e6f76464e7f5453d5379c30e9d6c5fae5f98f4e0
hyunjin <jin.2@kakaoent.com>
2025-12-21 18:29:01 +0900

    clean up files

--- document_load.py
+++
@@ -1,8 +0,0 @@
-from langchain.document_loaders import DirectoryLoader
-
-DATA_PATH = "data/books"
-
-def load_documents(data_path):
-    loader = DirectoryLoader(data_path, glob="*.md")
-    documents = loader.load()
-    return documents
